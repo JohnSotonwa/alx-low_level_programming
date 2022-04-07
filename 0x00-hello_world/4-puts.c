b8a98479d632fb4b1f451677b966417160bfc7b5
JohnSotonwa <johnsotonwa@gmail.com>
2022-04-07 16:52:41 -0700

    added comment and removed space for function name

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 # include <stdio.h>
-/*
+/**
  *main - the entry point
  *
  *return:program will execute successfully
  */
-int main(void) /*entry point*/
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
