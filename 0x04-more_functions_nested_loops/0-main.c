859691b5dcd6da1c5057216964e3bb2ad9ef2da6
JohnSotonwa <johnsotonwa@gmail.com>
2022-04-14 23:24:22 +0000

    changed comment

--- 0x04-more_functions_nested_loops/0-main.c
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -2,9 +2,9 @@
 #include <stdio.h>
 
 /**
- * _isupper - checks if parameter is an uppercase character.
- * @c: input character.
- * Return: 1 if is an uppercase character, 0 in other case.
+ * main - check the code.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
