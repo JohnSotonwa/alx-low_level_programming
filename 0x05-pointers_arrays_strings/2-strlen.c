aa456c39103a96cb943b3892a26df34ecbf1c07c
JohnSotonwa <johnsotonwa@gmail.com>
2022-04-20 23:29:45 +0000

    print length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/*
+ *int _strlen(char *s) - returns length of a string
+ *@s : string
+ *Return: no return
+ */
+int _strlen(char *s)
+{
+	/*counts length of string*/
+	int count = 0;
+
+	while (*(s + count) != '\0')
+		count++;
+	return (count);
+}
