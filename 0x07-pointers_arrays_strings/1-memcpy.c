8c5767bf36efad6696c9979e0c0db6e090b6cb05
JohnSotonwa <johnsotonwa@gmail.com>
2022-04-26 22:45:47 +0000

    Added ; to for loop

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,5 +1,6 @@
 #include "main.h"
-/*
+
+/**
  *_memcpy - copy n bytes from mem area src to dest
  *@src - mem area
  *@dest - destination
@@ -12,6 +13,6 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 	unsigned int i;
 
 	for (i=0, i<n, i++)
-		*(dest+i) = *(src+i)
+		*(dest+i) = *(src+i);
 	return (dest);
 }
